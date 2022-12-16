dab294810a5d69997c6e7dd0c10a3efc43134668
misael-reyes <misaelstark11@gmail.com>
2022-12-15 23:30:24 -0600

    card widget

--- fl_components/lib/screens/card_screen.dart
+++ fl_components/lib/screens/card_screen.dart
@@ -1,14 +1,33 @@
+import 'package:fl_components/theme/app_theme.dart';
 import 'package:flutter/material.dart';
 
 class CardScreen extends StatelessWidget {
-   
   const CardScreen({Key? key}) : super(key: key);
-  
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-         child: Text('CardScreen'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Card Widget"),
+      ),
+      body: ListView(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        children: [
+          // card widget, son parecidas a los item del recycler view
+          Card(
+            // colocamos un column porque queremos poner widget unos debajo de otros
+            child: Column(
+              children: const [
+                ListTile(
+                  leading: Icon(Icons.account_box_rounded,
+                      color: AppTheme.colorPrimary),
+                  title: Text("soy el titulo"),
+                  subtitle: Text("yo soy el subtitulo"),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
