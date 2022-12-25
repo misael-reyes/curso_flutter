fee91037919637294a13f866e1ec0363489cb16a
misael-reyes <misaelstark11@gmail.com>
2022-12-25 11:04:57 -0600

    82. mostrar una alerta

--- fl_components/lib/screens/alert_screen.dart
+++ fl_components/lib/screens/alert_screen.dart
@@ -3,6 +3,36 @@ import 'package:flutter/material.dart';
 class AlertScreen extends StatelessWidget {
   const AlertScreen({Key? key}) : super(key: key);
 
+  void displayDialog(BuildContext context) {
+    showDialog(
+      // esto nos permite cerrar el dialog en la sombra
+      barrierDismissible: false,
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadiusDirectional.circular(10)),
+          elevation: 5,
+          title: const Text('titulo del dialogo'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: const [
+              Text('este es el contenido del dialog'),
+              SizedBox(height: 10),
+              FlutterLogo(size: 100),
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.pop(context),
+              child: const Text('Cancelar'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,9 +51,9 @@ class AlertScreen extends StatelessWidget {
               style: TextStyle(fontSize: 18),
             ),
           ),
-          onPressed: () {
-            //
-          },
+          // lo podemos hacer de dos maneras
+          onPressed: () => displayDialog(context),
+          //onPressed: displayAlert,
         ),
       ),
       floatingActionButton: FloatingActionButton(
