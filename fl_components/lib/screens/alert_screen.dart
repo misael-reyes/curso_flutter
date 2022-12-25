c1ff34a5aca78842dfc479777a1306a42067e73f
misael-reyes <misaelstark11@gmail.com>
2022-12-25 11:47:38 -0600

    83. dialogo cupertino para IOS

--- fl_components/lib/screens/alert_screen.dart
+++ fl_components/lib/screens/alert_screen.dart
@@ -1,9 +1,46 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class AlertScreen extends StatelessWidget {
   const AlertScreen({Key? key}) : super(key: key);
 
-  void displayDialog(BuildContext context) {
+  void displayDialogIOS(BuildContext context) {
+    showCupertinoDialog(
+      barrierDismissible: false,
+      context: context,
+      builder: (context) {
+        return CupertinoAlertDialog(
+          title: const Text('Título'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: const [
+              Text('este es el contenido del dialog'),
+              SizedBox(height: 10),
+              FlutterLogo(size: 100),
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.pop(context),
+              child: const Text(
+                'Cancelar',
+                style: TextStyle(color: Colors.red),
+              ),
+            ),
+            TextButton(
+              onPressed: () => Navigator.pop(context),
+              child: const Text('Aceptar'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  // esto está bien para android
+  void displayDialogAndroid(BuildContext context) {
     showDialog(
       // esto nos permite cerrar el dialog en la sombra
       barrierDismissible: false,
@@ -25,7 +62,14 @@ class AlertScreen extends StatelessWidget {
           actions: [
             TextButton(
               onPressed: () => Navigator.pop(context),
-              child: const Text('Cancelar'),
+              child: const Text(
+                'Cancelar',
+                style: TextStyle(color: Colors.red),
+              ),
+            ),
+            TextButton(
+              onPressed: () => Navigator.pop(context),
+              child: const Text('Aceptar'),
             ),
           ],
         );
@@ -38,23 +82,26 @@ class AlertScreen extends StatelessWidget {
     return Scaffold(
       body: Center(
         child: ElevatedButton(
-          // si este boton es especial, podemos sobreescribir su estilo
-          // lo dejo comentado como referencia
-          // style: ElevatedButton.styleFrom(
-          //   backgroundColor: Colors.indigo,
-          //   elevation: 50,
-          // ),
-          child: const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
-            child: Text(
-              'Mostrar alerta',
-              style: TextStyle(fontSize: 18),
+            // si este boton es especial, podemos sobreescribir su estilo
+            // lo dejo comentado como referencia
+            // style: ElevatedButton.styleFrom(
+            //   backgroundColor: Colors.indigo,
+            //   elevation: 50,
+            // ),
+            child: const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+              child: Text(
+                'Mostrar alerta',
+                style: TextStyle(fontSize: 18),
+              ),
             ),
-          ),
-          // lo podemos hacer de dos maneras
-          onPressed: () => displayDialog(context),
-          //onPressed: displayAlert,
-        ),
+            // lo podemos hacer de dos maneras
+            //onPressed: () => displayDialogAndroid(context),
+            onPressed: () => Platform.isAndroid
+                ? displayDialogAndroid(context)
+                : displayDialogIOS(context)
+            //onPressed: displayAlert,
+            ),
       ),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.close),
