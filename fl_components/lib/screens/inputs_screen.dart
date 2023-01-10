eaef7d556505f4f4ae20a0f1aef2942fa095e6f7
misael-reyes <misaelstark11@gmail.com>
2023-01-10 09:45:39 -0600

    88. TextFormFields Personalización

--- fl_components/lib/screens/inputs_screen.dart
+++ fl_components/lib/screens/inputs_screen.dart
@@ -19,14 +19,41 @@ class InputsScreen extends StatelessWidget {
             children: [
               // se recomienda usar el siguiente widget para formularios
               TextFormField(
-                autofocus: true,
-                initialValue: 'Misael Reyes',
+                // esto es para ya tener el puntero sobre el textformfield
+                autofocus: false,
+                initialValue: '',
                 // esto pone el teclado en mayúscula antes de cada palabra
                 textCapitalization: TextCapitalization.words,
                 // capturamos el evento cada que una tecla del teclado se precione
                 onChanged: (value) {
                   print('value $value');
                 },
+                validator: (value) {
+                  if (value == null) return 'Este campo es obligatorio';
+                  return value.length < 3 ? 'Minimo tres' : null;
+                },
+                //autovalidateMode: AutovalidateMode.always,
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                decoration: const InputDecoration(
+                  hintText: 'Nombre del usuario',
+                  labelText: 'Nombre',
+                  helperText: 'Solo letras',
+                  counterText: 'Tres caracteres',
+                  prefixIcon: Icon(Icons.usb_rounded),
+                  suffixIcon: Icon(Icons.group),
+                  icon: Icon(Icons.admin_panel_settings),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.green,
+                    ),
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(10),
+                      topRight: Radius.circular(10),
+                    ),
+                  ),
+                ),
               )
             ],
           ),
