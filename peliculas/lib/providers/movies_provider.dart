4fc3fd8e1cb92ecf370ccd27c1bef79fa95fe5a9
misael-reyes <misaelstark11@gmail.com>
2023-02-18 10:35:19 -0600

    117. Peticiones HTTP

--- peliculas/lib/providers/movies_provider.dart
+++ peliculas/lib/providers/movies_provider.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/cupertino.dart';
+// esto es de la libreria http
+import 'package:http/http.dart' as http;
 
 /// algunos le ponen de nombre services, pero la idea es que funcione
 /// como un proveedor de informacion
@@ -6,6 +10,11 @@ import 'package:flutter/cupertino.dart';
 /// de ChangeNotifier
 
 class MoviesProvider extends ChangeNotifier {
+  //
+  String _baseUrl = 'api.themoviedb.org';
+  String _apiKey = '59d52311717635a679931ce80df710d6';
+  String _language = 'es-ES';
+
   MoviesProvider() {
     print('Movies provider inicializado');
     getOnDisplayMovies();
@@ -13,6 +22,17 @@ class MoviesProvider extends ChangeNotifier {
 
   // funicion para traernos a todas las peliculas que mostraremos
   getOnDisplayMovies() async {
-    print('getOnDisplayMovies');
+    var url = Uri.https(
+        _baseUrl, // dominio
+        '3/movie/now_playing', // segmento
+        {'api_key': _apiKey, 'language': _language, 'page': '1'});
+
+    // Await the http get response, then decode the json-formatted response.
+    final response = await http.get(url);
+
+    // convertimos la respuesta en un mapa
+    final Map<String, dynamic> decodedData = json.decode(response.body);
+
+    print(decodedData);
   }
 }
