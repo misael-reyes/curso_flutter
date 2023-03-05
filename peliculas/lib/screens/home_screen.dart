7850035d0550fdd63d755a1af7cf6ca00bcddde9
misael-reyes <misaelstark11@gmail.com>
2023-03-05 11:31:07 -0600

    128. Implementar un SearchDelegate

--- peliculas/lib/screens/home_screen.dart
+++ peliculas/lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:peliculas/providers/movies_provider.dart';
+import 'package:peliculas/search/search_delegate.dart';
 import 'package:peliculas/widgets/widgets.dart';
 import 'package:provider/provider.dart';
 
@@ -18,7 +19,12 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Películas en cines'),
-        actions: [IconButton(onPressed: () {}, icon: const Icon(Icons.search))],
+        actions: [
+          IconButton(
+            onPressed: () => showSearch(context: context, delegate: MovieSearchDelegate()), 
+            icon: const Icon(Icons.search)
+          )
+        ],
       ),
 
       /// como nuestro colum es muy alto, nos marca un error en la pantalla porque no se
