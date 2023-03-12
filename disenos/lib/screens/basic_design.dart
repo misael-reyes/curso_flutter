58ebd1ff58b5ce8680dba518e3269d302bbd5b38
misael-reyes <misaelstark11@gmail.com>
2023-03-11 18:44:29 -0600

    142. diseño basico parte 2

--- disenos/lib/screens/basic_design.dart
+++ disenos/lib/screens/basic_design.dart
@@ -1,14 +1,46 @@
 import 'package:flutter/material.dart';
 
 class BasicDesignScreen extends StatelessWidget {
-   
   const BasicDesignScreen({Key? key}) : super(key: key);
-  
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-         child: Image(image: AssetImage('assets/paisaje.jpg'))
+    return Scaffold(
+        body: Column(
+      children: const [
+        // Imagen
+        Image(image: AssetImage('assets/paisaje.jpg')),
+        // titulo
+        Title()
+      ],
+    ));
+  }
+}
+
+class Title extends StatelessWidget {
+  const Title({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+      padding: const EdgeInsets.only(left: 10, right: 20, top: 5, bottom: 5),
+      color: Colors.green,
+      child: Row(
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: const [
+              Text('Oeschinen Lake Campground', style: TextStyle(fontWeight: FontWeight.bold)),
+              Text('Kandersteg Switzerland', style: TextStyle(color: Colors.black45))
+            ],
+          ),
+          Expanded(child: Container()),
+          const Icon(Icons.star, color: Colors.red),
+          const Text('41')
+        ],
       ),
     );
   }
