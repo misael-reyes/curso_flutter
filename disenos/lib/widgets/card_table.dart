8d0b543a84fcab446bb4e78f7e0419abee2615a6
misael-reyes <misaelstark11@gmail.com>
2023-03-18 19:38:28 -0600

    153. Optimizaciones posibles

--- disenos/lib/widgets/card_table.dart
+++ disenos/lib/widgets/card_table.dart
@@ -10,15 +10,13 @@ class CardTable extends StatelessWidget {
     return Table(
       children: const [
         // todos los table rows que pongamos deben de tener la misma cantidad de hijos
-        TableRow(
-          children: [
-            _SigleCard(icon: Icons.ac_unit_rounded, color: Colors.blueAccent, label: 'General'), 
-            _SigleCard(icon: Icons.access_time_outlined, color: Colors.purple, label: 'Transport')
-          ]
-        ),
+        TableRow(children: [
+          _SigleCard(icon: Icons.ac_unit_rounded, color: Colors.blueAccent, label: 'General'),
+          _SigleCard(icon: Icons.access_time_outlined, color: Colors.purple, label: 'Transport')
+        ]),
         TableRow(children: [
           _SigleCard(icon: Icons.agriculture, color: Colors.pink, label: 'Shoping'),
-          _SigleCard(icon: Icons.account_tree, color: Colors.orange, label:'Bills')
+          _SigleCard(icon: Icons.account_tree, color: Colors.orange, label: 'Bills')
         ]),
         TableRow(children: [
           _SigleCard(icon: Icons.catching_pokemon_sharp, color: Colors.blue, label: 'Entertainment'),
@@ -36,49 +34,52 @@ class CardTable extends StatelessWidget {
 // widget que serán los botones
 
 class _SigleCard extends StatelessWidget {
-
   // atributos que tendran que pasarnos para crear este widget
   final IconData icon;
   final Color color;
   final String label;
 
-  const _SigleCard({
-    super.key,
-    required this.icon,
-    required this.color,
-    required this.label
-  });
+  const _SigleCard({super.key, required this.icon, required this.color, required this.label});
 
   @override
   Widget build(BuildContext context) {
-
-    var boxDecoration = BoxDecoration(
-      color: const Color.fromRGBO(62, 66, 107, 0.7),
-      borderRadius: BorderRadius.circular(20)
+    var column = Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        CircleAvatar(
+          backgroundColor: color,
+          radius: 30,
+          child: Icon(icon, size: 35, color: Colors.white),
+        ),
+        const SizedBox(height: 20),
+        Text(label, style: TextStyle(color: color, fontSize: 16))
+      ],
     );
+    return _CardBackground(child: column);
+  }
+}
 
+class _CardBackground extends StatelessWidget {
+  final Widget child;
+
+  const _CardBackground({super.key, required this.child});
+
+  @override
+  Widget build(BuildContext context) {
     return Container(
       margin: const EdgeInsets.all(5),
       // este widget corta todo lo que salga de su espacio
-      child: ClipRRect (
+      child: ClipRRect(
         borderRadius: BorderRadius.circular(20),
         child: BackdropFilter(
           filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
           child: Container(
             height: 180,
-            decoration: boxDecoration,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                CircleAvatar(
-                  backgroundColor: color,
-                  radius: 30,
-                  child: Icon(icon, size: 35, color: Colors.white),
-                ),
-                const SizedBox(height: 20),
-                Text(label, style: TextStyle(color: color, fontSize: 16))
-              ],
+            decoration: BoxDecoration(
+              color: const Color.fromRGBO(62, 66, 107, 0.7), 
+              borderRadius: BorderRadius.circular(20)
             ),
+            child: child,
           ),
         ),
       ),
