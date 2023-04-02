3cd282879ad669fb16314499bb58e1a93195deed
misael-reyes <misaelstark11@gmail.com>
2023-04-02 14:42:49 -0600

    nuevo proyecto

---
+++ hello_world_app/lib/main.dart
@@ -0,0 +1,21 @@
+
+import 'package:flutter/material.dart';
+import 'package:hello_world_app/presentation/screens/counter/counter_screen.dart';
+
+void main() {
+  runApp( const MyApp() );
+}
+
+class MyApp extends StatelessWidget {
+  //
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: CounterScreen()
+    );
+  }
+}
