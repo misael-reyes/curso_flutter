5bc91883523a5d7fe11f28aafcbd47bbda2c3671
misael-reyes <misaelstark11@gmail.com>
2023-04-04 01:52:40 -0600

    cambiar el estado de la aplicación

--- hello_world_app/lib/presentation/screens/counter/counter_screen.dart
+++ hello_world_app/lib/presentation/screens/counter/counter_screen.dart
@@ -1,7 +1,17 @@
 import 'package:flutter/material.dart';
 
-class CounterScreen extends StatelessWidget {
+class CounterScreen extends StatefulWidget {
+  // el constructor es const porque flutter lo maneja como inmutable (nunca va a cambiar)
   const CounterScreen({super.key});
+
+  // creación del estado
+  @override
+  State<CounterScreen> createState() => _CounterScreenState();
+}
+
+// widget visual
+class _CounterScreenState extends State<CounterScreen> {
+  int clickCounter = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -13,12 +23,12 @@ class CounterScreen extends StatelessWidget {
       body: Center(
           child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
-        children: const [
+        children: [
           Text(
-            '10',
-            style: TextStyle(fontSize: 160, fontWeight: FontWeight.w100),
+            '$clickCounter',
+            style: const TextStyle(fontSize: 160, fontWeight: FontWeight.w100),
           ),
-          Text(
+          const Text(
             'Clicks',
             style: TextStyle(fontSize: 25, fontWeight: FontWeight.w100),
           )
@@ -26,7 +36,11 @@ class CounterScreen extends StatelessWidget {
       )),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          //
+          clickCounter++;
+          // indicamos que se tiene que renderizar nuestro widget
+          setState(() {
+            // tambien podemos poner aqui la instrucción clickCounter ++;
+          });
         },
         child: const Icon(Icons.plus_one),
       ),
