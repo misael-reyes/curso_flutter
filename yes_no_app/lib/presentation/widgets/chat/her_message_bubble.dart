065ee27d6367213f238ff527351b34814f65c74a
misael-reyes <misaelstark11@gmail.com>
2023-04-06 17:10:15 -0600

    Mostrar mensaje mientras se carga la imagen

--- yes_no_app/lib/presentation/widgets/chat/her_message_bubble.dart
+++ yes_no_app/lib/presentation/widgets/chat/her_message_bubble.dart
@@ -40,14 +40,22 @@ class _ImageBubble extends StatelessWidget {
 
     // ClipRRect nos permite hacer bordes redondeados
     return ClipRRect(
-      borderRadius: BorderRadius.circular(20),
-      child: Image.network(
+        borderRadius: BorderRadius.circular(20),
+        child: Image.network(
           'https://yesno.wtf/assets/yes/3-422e51268d64d78241720a7de52fe121.gif',
           width: size.width * 0.7,
           height: 150,
           // con boxfit decimos que basado en las dimensiones que le estamos dando coloque la imagen
-          fit: BoxFit.cover,  
-        )
-    );
+          fit: BoxFit.cover,
+          loadingBuilder: (context, child, loadingProgress) {
+            if (loadingProgress == null) return child;
+            return Container(
+              width: size.width * 0.7,
+              height: 150,
+              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+              child: const Text('mi amor esta enviando un mensaje'),
+            );
+          },
+        ));
   }
 }
