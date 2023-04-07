fafeda8440ad8a42346e3740fccd0e45867f4665
misael-reyes <misaelstark11@gmail.com>
2023-04-07 15:52:51 -0600

    Comportamiento del FormField

--- yes_no_app/lib/presentation/widgets/shared/message_field_box.dart
+++ yes_no_app/lib/presentation/widgets/shared/message_field_box.dart
@@ -5,30 +5,48 @@ class MessageFieldBox extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    //
+
+    // esto es para matener el focus (teclado) cuando precionamos el enter o la palomita
+    final FocusNode focusNode = FocusNode();
+
+    final textController = TextEditingController();
+
     final outlineInputBorder = UnderlineInputBorder(
         borderSide: const BorderSide(color: Colors.transparent),
         borderRadius: BorderRadius.circular(40));
 
     final inputDecoration = InputDecoration(
+      hintText: 'End your message with a "?"',
       enabledBorder: outlineInputBorder,
       focusedBorder: outlineInputBorder,
       filled: true,
       suffixIcon: IconButton(
         icon: const Icon(Icons.send),
         onPressed: () {
-          print('valor de la caja de texto');
+          final textValue = textController.value.text;
+          print('button $textValue');
+          textController.clear();
         },
       ),
     );
 
     return TextFormField(
+      // esto es para que al dar clic fuera del teclado se oculte
+      onTapOutside: (event) {
+        focusNode.unfocus();
+      },
+      focusNode: focusNode,
+      controller: textController,
       // aplicamos estilos
       decoration: inputDecoration,
+      // esto se ejecutará cuando presionamos enter
       onFieldSubmitted: (value) {
         print('submit value $value');
-      },
-      onChanged: (value) {
-        print('changed: $value');
+        // limipiamos el imput
+        textController.clear();
+        // mantenemos el focus
+        focusNode.requestFocus();
       },
     );
   }
