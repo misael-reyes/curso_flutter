c6aa466c1997feebdfd2d7ba15de396e53c3c0b5
misael-reyes <misaelstark11@gmail.com>
2023-04-12 13:09:24 -0600

    quickType

--- yes_no_app/lib/config/helpers/get_yes_no_answer.dart
+++ yes_no_app/lib/config/helpers/get_yes_no_answer.dart
@@ -19,9 +19,6 @@ class GetYesNoAnswer {
 
     final yesNoModel = YesNoModel.fromJsonMap(response.data);
 
-    return Message(
-        text: yesNoModel.answer,
-        fromWho: FromWho.hers,
-        imageUrl: yesNoModel.image);
+    return yesNoModel.toMessageEntity();
   }
 }
--- yes_no_app/lib/infrastructure/models/yes_no_model.dart
+++ yes_no_app/lib/infrastructure/models/yes_no_model.dart
@@ -1,22 +1,34 @@
+import 'package:yes_no_app/domain/entities/message.dart';
+
 /// el objetivo de este modelo no es más que tener todas las propiedades
 /// del modelo como lo trae nuestra respuesta del servidor
 
 class YesNoModel {
-  String answer;
-  bool forced;
-  String image;
-
   YesNoModel({
     required this.answer,
     required this.forced,
     required this.image,
   });
 
-  // constructor con nombre
-  factory YesNoModel.fromJsonMap(Map<String, dynamic> json) =>
-      YesNoModel(
-        answer: json['answer'], 
-        forced: json['forced'], 
-        image: json['image']
+  final String answer;
+  final bool forced;
+  final String image;
+
+  factory YesNoModel.fromJsonMap(Map<String, dynamic> json) => YesNoModel(
+        answer: json["answer"],
+        forced: json["forced"],
+        image: json["image"],
       );
+
+  Map<String, dynamic> toJson() => {
+        "answer": answer,
+        "forced": forced,
+        "image": image,
+      };
+
+  Message toMessageEntity() => Message(
+    text: answer == 'yes' ? 'Si' : 'No', 
+    fromWho: FromWho.hers,
+    imageUrl: image
+  );
 }
