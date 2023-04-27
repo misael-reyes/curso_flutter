f7a13aacfb19274737202b94a50b1ea18c2d189c
misael-reyes <misaelstark11@gmail.com>
2023-04-27 12:29:05 -0600

    Tarjetas con border

--- widgets_app/lib/presentation/screens/cards/cards_screen.dart
+++ widgets_app/lib/presentation/screens/cards/cards_screen.dart
@@ -38,7 +38,9 @@ class _CardsView extends StatelessWidget {
         children: [
           ...cards.map(
             (card) => _CardType1(label: card['label'], elevation: card['elevation'])
-          )
+          ),
+          ...cards.map(
+              (card) => _CardType2(label: card['label'], elevation: card['elevation']))
         ],
       ),
     );
@@ -74,6 +76,46 @@ class _CardType1 extends StatelessWidget {
               child: Text(label),
             )
             
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _CardType2 extends StatelessWidget {
+  
+  final String label;
+  final double elevation;
+
+  const _CardType2({required this.label, required this.elevation});
+
+  @override
+  Widget build(BuildContext context) {
+    
+    final colors = Theme.of(context).colorScheme;
+    
+    return Card(
+      // al usar este border, el color cambiará de acuerdo al tema seleccionado
+      shape: RoundedRectangleBorder(
+        borderRadius: const BorderRadius.all(Radius.circular(12)),
+        side: BorderSide(
+          color: colors.outline
+        )
+      ),
+      elevation: elevation,
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(10, 5, 10, 10),
+        child: Column(
+          children: [
+            Align(
+                alignment: Alignment.topRight,
+                child: IconButton(
+                    onPressed: () {}, icon: const Icon(Icons.more_vert_outlined))),
+            Align(
+              alignment: Alignment.bottomLeft,
+              child: Text(label),
+            )
           ],
         ),
       ),
