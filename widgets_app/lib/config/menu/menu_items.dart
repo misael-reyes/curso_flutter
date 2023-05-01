791e4521ca0f69661b52a4dc28fbf8305a9537a9
misael-reyes <misaelstark11@gmail.com>
2023-05-01 13:15:22 -0600

    Checkbox, Radios y otros Tiles

--- widgets_app/lib/config/menu/menu_items.dart
+++ widgets_app/lib/config/menu/menu_items.dart
@@ -16,29 +16,41 @@ class MenuItem {
 }
 
 const appMenuItems = <MenuItem>[
+  
   MenuItem(
       title: 'Botones',
       subTitle: 'Varios botones en flutter',
       link: '/buttons',
       icon: Icons.smart_button_outlined),
+  
   MenuItem(
       title: 'Tarjetas',
       subTitle: 'Un contenedor estilizado',
       link: '/cards',
       icon: Icons.credit_card),
+  
   MenuItem(
       title: 'ProgressIndicators',
       subTitle: 'Generales y controlados',
       link: '/progress',
       icon: Icons.refresh_rounded),
+  
   MenuItem(
       title: 'Snackbars y diálogos',
       subTitle: 'Indicadores en pantalla',
       link: '/snackbars',
       icon: Icons.info_outline),
+      
   MenuItem(
       title: 'Animated container',
       subTitle: 'Stateful widget animado',
       link: '/animated',
-      icon: Icons.check_box_outline_blank_sharp)
+      icon: Icons.check_box_outline_blank_sharp),
+      
+  MenuItem(
+      title: 'UI Controls + Tiles',
+      subTitle: 'Una serie de controles en flutter',
+      link: '/ui-controls',
+      icon: Icons.car_rental_outlined)
+
 ];
--- widgets_app/lib/presentation/screens/ui_controls/ui_controls_screen.dart
+++ widgets_app/lib/presentation/screens/ui_controls/ui_controls_screen.dart
@@ -1,13 +1,90 @@
 import 'package:flutter/material.dart';
 
 class UiControlsScreen extends StatelessWidget {
-  
   static const name = 'ui_controls_screen';
 
   const UiControlsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('UI Controls'),
+        ),
+        body: const _UiControlsView());
+  }
+}
+
+class _UiControlsView extends StatefulWidget {
+  const _UiControlsView();
+
+  @override
+  State<_UiControlsView> createState() => _UiControlsViewState();
+}
+
+enum Transportation { car, plane, boat, submarine }
+
+class _UiControlsViewState extends State<_UiControlsView> {
+  bool isDeveloper = true;
+  Transportation selectedTransportation = Transportation.car;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      // esto es para evitar el movimiento de rebote que hace en IOS
+      physics: const ClampingScrollPhysics(),
+      children: [
+        /// usamos SwitchListTile porque nos permite cambiar el estado
+        /// del switch solo con tocar el titulo, o toda la linea
+        SwitchListTile(
+          title: const Text('Developer Mode'),
+          subtitle: const Text('Controles adicionales'),
+          value: isDeveloper,
+          onChanged: (value) => setState(() {
+            isDeveloper = !isDeveloper;
+          })
+        ),
+
+        RadioListTile(
+          title: const Text('By Car'),
+          subtitle: const Text('Viajar por carro'),
+          value: Transportation.car,
+          groupValue: selectedTransportation,
+          onChanged: (value) => setState(() {
+            selectedTransportation = Transportation.car;
+          })
+        ),
+
+        RadioListTile(
+          title: const Text('By Boat'),
+          subtitle: const Text('Viajar por barco'),
+          value: Transportation.boat,
+          groupValue: selectedTransportation,
+          onChanged: (value) => setState(() {
+            selectedTransportation = Transportation.boat;
+          })
+        ),
+
+        RadioListTile(
+          title: const Text('By Plane'),
+          subtitle: const Text('Viajar por avión'),
+          value: Transportation.plane,
+          groupValue: selectedTransportation,
+          onChanged: (value) => setState(() {
+            selectedTransportation = Transportation.plane;
+          })
+        ),
+
+        RadioListTile(
+          title: const Text('By Submarine'),
+          subtitle: const Text('Viajar por submarino'),
+          value: Transportation.submarine,
+          groupValue: selectedTransportation,
+          onChanged: (value) => setState(() {
+            selectedTransportation = Transportation.submarine;
+          })
+        ),
+      ],
+    );
   }
 }
