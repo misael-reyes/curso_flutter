c9ac1dc17e21d29f736e42f9ab05efd402c9fac2
misael-reyes <misaelstark11@gmail.com>
2023-05-14 13:54:41 -0600

    MovieMappers - MovieDb hacia movie entitty

---
+++ cinemapedia/lib/infrastructure/mappers/movie_mapper.dart
@@ -0,0 +1,28 @@
+import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/infrastructure/models/moviedb/movie_moviedb.dart';
+
+/// La idea de esta clase es crear un objeto de tipo Movie partiendo
+/// de un objeto de tipo MovieMovieDB o cualquier otra fuente que nos
+/// pasen
+
+class MovieMapper {
+  static Movie movieDBToEntity(MovieMovieDB moviedb) => Movie(
+      adult: moviedb.adult,
+      backdropPath: (moviedb.backdropPath != '')
+        ? 'https://image.tmdb.org/t/p/w500${ moviedb.backdropPath }'
+        : 'https://www.electiondataservices.com/wp-content/uploads/2014/10/sorry-image-not-available.jpg',
+      genreIds: moviedb.genreIds.map((e) => e.toString()).toList(),
+      id: moviedb.id,
+      originalLanguage: moviedb.originalLanguage,
+      originalTitle: moviedb.originalTitle,
+      overview: moviedb.overview,
+      popularity: moviedb.popularity,
+      posterPath: (moviedb.posterPath != '')
+        ? 'https://image.tmdb.org/t/p/w500${ moviedb.posterPath }'
+        : 'https://www.movienewz.com/img/films/poster-holder.jpg',
+      releaseDate: moviedb.releaseDate,
+      title: moviedb.title,
+      video: moviedb.video,
+      voteAverage: moviedb.voteAverage,
+      voteCount: moviedb.voteCount);
+}
