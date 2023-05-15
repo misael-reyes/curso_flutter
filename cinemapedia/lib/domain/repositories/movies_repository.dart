401e4e568ab71b885733b02e45d2c146e7f84d0e
misael-reyes <misaelstark11@gmail.com>
2023-05-14 20:47:22 -0600

    MovieRepository - implementación

--- cinemapedia/lib/domain/repositories/movies_repository.dart
+++ cinemapedia/lib/domain/repositories/movies_repository.dart
@@ -3,7 +3,7 @@ import 'package:cinemapedia/domain/entities/movie.dart';
 
 // los repositorios son los que llaman a los datasources
 
-abstract class MovieRepository {
+abstract class MoviesRepository {
 
   Future<List<Movie>> getNowPlaying({ int page = 1 });
 }
