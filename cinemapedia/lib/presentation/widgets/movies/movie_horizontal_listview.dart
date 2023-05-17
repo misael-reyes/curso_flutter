da26c7d3564d3d2f5f16bb55cbc85a1a51133871
misael-reyes <misaelstark11@gmail.com>
2023-05-17 12:42:59 -0600

    Mostrar las peliculas en el ListView

--- cinemapedia/lib/presentation/widgets/movies/movie_horizontal_listview.dart
+++ cinemapedia/lib/presentation/widgets/movies/movie_horizontal_listview.dart
@@ -1,4 +1,5 @@
 
+import 'package:animate_do/animate_do.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
 import 'package:flutter/material.dart';
 
@@ -33,7 +34,20 @@ class MovieHorizontalListview extends StatelessWidget {
         children: [
 
           if (title != null || subTitle != null)
-            _Title(title: title, subTitle: subTitle)
+            _Title(title: title, subTitle: subTitle),
+
+          // Expanded(
+            //child: Text('holamundo')
+          //)
+          Expanded(
+            child: ListView.builder(
+              itemCount: movies.length,
+              scrollDirection: Axis.horizontal,
+              // especificamos la fisica para que sea igual en iOS y Android
+              physics: const BouncingScrollPhysics(),
+              itemBuilder: (context, index) => _Slide(movie: movies[index]),
+            ),
+          )
 
         ],
       ),
@@ -54,7 +68,7 @@ class _Title extends StatelessWidget {
     final textStyle = Theme.of(context).textTheme.titleLarge;
 
     return Container(
-      color: Colors.red,
+      //color: Colors.red,
       padding: const EdgeInsets.only(top: 10),
       margin: const EdgeInsets.symmetric(horizontal: 10),
       child: Row(
@@ -70,6 +84,81 @@ class _Title extends StatelessWidget {
               onPressed: (){}, 
               child: Text(subTitle!)
             )
+
+        ],
+      ),
+    );
+  }
+}
+
+class _Slide extends StatelessWidget {
+
+  final Movie movie;
+
+  const _Slide({ required this.movie });
+
+  @override
+  Widget build(BuildContext context) {
+
+    final textStyle = Theme.of(context).textTheme;
+
+    // elegimos un container porque podemos colocar margenes
+    return Container(
+      //color: Colors.blue,
+      margin: const EdgeInsets.symmetric(horizontal: 8),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+
+          SizedBox(
+            width: 150,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: Image.network(
+                movie.posterPath,
+                fit: BoxFit.cover,
+                width: 150,
+                // el loading builder es cuando ya se contruye la imagen ejecutamos esto
+                loadingBuilder: (context, child, loadingProgress) {
+                  if(loadingProgress != null) {
+                    return const Padding(
+                      padding: EdgeInsets.all(10),
+                      child: Center(
+                        child: CircularProgressIndicator( strokeWidth: 2 )
+                      ),
+                    );
+                  }
+                  return FadeIn(child: child);
+                },
+              ),
+            ),
+          ), 
+          
+          const SizedBox(height: 5),
+
+          // Title
+          SizedBox(
+            width: 150,
+            child: Text(
+              movie.title, 
+              maxLines: 2,
+              style: textStyle.titleSmall,
+            ),
+          ),
+
+          // Rating
+          SizedBox(
+            width: 150,
+            child: Row(
+              children: [
+                Icon(Icons.star_half_outlined, color: Colors.yellow.shade800),
+                const SizedBox(width: 3),
+                Text('${movie.voteAverage}', style: textStyle.bodyMedium?.copyWith(color: Colors.yellow.shade800)),
+                const SizedBox(width: 10),
+                Text('${movie.popularity}', style: textStyle.bodySmall)
+              ],
+            ),
+          )
 
         ],
       ),
