4918dfd3b4bf55d8433d0ecb89686dddf051a2af
misael-reyes <misaelstark11@gmail.com>
2023-05-18 11:06:09 -0600

    CustomScrollBar

--- cinemapedia/lib/presentation/screens/movies/home_screen.dart
+++ cinemapedia/lib/presentation/screens/movies/home_screen.dart
@@ -1,30 +1,24 @@
-
 import 'package:cinemapedia/presentation/providers/providers.dart';
 import 'package:cinemapedia/presentation/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class HomeScreen extends StatelessWidget {
-
   static const name = 'home-screen';
-  
-  const HomeScreen ({super.key});
+
+  const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: _HomeView(),
-      bottomNavigationBar: CustomBottomNavigationbar()
-    );
+        body: _HomeView(), bottomNavigationBar: CustomBottomNavigationbar());
   }
 }
 
-
-/// lo convertimos primero a un statefulwidget porque queremos usar 
+/// lo convertimos primero a un statefulwidget porque queremos usar
 /// el initState, pero como estamos usando riverpod ahora lo convertimos
 /// a ConsumerStatefulWidget y no a ConsumerWidget
 class _HomeView extends ConsumerStatefulWidget {
-
   const _HomeView();
 
   @override
@@ -33,7 +27,6 @@ class _HomeView extends ConsumerStatefulWidget {
 
 // ahora no será State sino ConsumerState y ahora tenemos acceso a ref en toda la clase _HomeViewState
 class _HomeViewState extends ConsumerState<_HomeView> {
-
   @override
   void initState() {
     super.initState();
@@ -45,40 +38,66 @@ class _HomeViewState extends ConsumerState<_HomeView> {
 
   @override
   Widget build(BuildContext context) {
-
     // aqui tenemos las 20 peliculas
-    final nowPlayingMovies = ref.watch( nowPlayingMoviesProvider );
+    final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider);
 
     // en este ya estan solo las 6 peliculas
-    final slideShowMovies = ref.watch( moviesSlideshowProvider );
+    final slideShowMovies = ref.watch(moviesSlideshowProvider);
 
-    return SingleChildScrollView(
-      child: Column(
-        children: [
-    
-          const CustomAppbar(),
-    
-          /// queremos pasar solo 6 peliculas al slider, si decimos que corte el arreglo
-          /// marcara un error porque al inicio el arreglo esta vacio, entonces para
-          /// solucioar esto decidimos crear un nuevo provider
-          MoviesSlideShow(movies: slideShowMovies),
-    
-          MovieHorizontalListview(
-            movies: nowPlayingMovies,
-            title: 'En cines',
-            subTitle: 'Lunes 20',
-            loadNextPage: () => ref.read(nowPlayingMoviesProvider.notifier).loadNextPage()
+    // los slivers siempre trabajarán con un CustomScrollView
+    return CustomScrollView(
+        // los slivers son widgets que trabajan con caracteristicas de los scrollview
+        slivers: [
+
+          const SliverAppBar(
+            floating: true,
+            flexibleSpace: FlexibleSpaceBar(
+              centerTitle: true,
+              title: CustomAppbar()
+            ),
           ),
-    
-          MovieHorizontalListview(
-            movies: nowPlayingMovies,
-            title: 'En cines',
-            subTitle: 'Lunes 20',
-            loadNextPage: () => ref.read(nowPlayingMoviesProvider.notifier).loadNextPage()
-          ),
-    
-        ],
-      ),
+
+          SliverList(delegate: SliverChildBuilderDelegate((context, index) {
+            return Column(
+              children: [
+                //const CustomAppbar(),
+
+                /// queremos pasar solo 6 peliculas al slider, si decimos que corte el arreglo
+                /// marcara un error porque al inicio el arreglo esta vacio, entonces para
+                /// solucioar esto decidimos crear un nuevo provider
+                MoviesSlideShow(movies: slideShowMovies),
+
+                MovieHorizontalListview(
+                    movies: nowPlayingMovies,
+                    title: 'En cines',
+                    subTitle: 'Lunes 20',
+                    loadNextPage: () =>
+                        ref.read(nowPlayingMoviesProvider.notifier).loadNextPage()),
+
+                MovieHorizontalListview(
+                    movies: nowPlayingMovies,
+                    title: 'Próximamente',
+                    subTitle: 'Este mes',
+                    loadNextPage: () =>
+                        ref.read(nowPlayingMoviesProvider.notifier).loadNextPage()),
+
+                MovieHorizontalListview(
+                    movies: nowPlayingMovies,
+                    title: 'Populares',
+                    // subTitle: 'Lunes 20',
+                    loadNextPage: () =>
+                        ref.read(nowPlayingMoviesProvider.notifier).loadNextPage()),
+
+                MovieHorizontalListview(
+                    movies: nowPlayingMovies,
+                    title: 'Mejor calificadas',
+                    subTitle: 'Desde siempre',
+                    loadNextPage: () =>
+                        ref.read(nowPlayingMoviesProvider.notifier).loadNextPage()),
+              ],
+            );
+          }, childCount: 1))
+        ]
     );
   }
 }
