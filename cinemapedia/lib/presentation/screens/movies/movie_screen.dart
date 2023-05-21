56db5cdc4c1c327873a48363e9d9647d37405352
misael-reyes <misaelstark11@gmail.com>
2023-05-21 01:26:37 -0600

    Descripción de la pelicula

--- cinemapedia/lib/presentation/screens/movies/movie_screen.dart
+++ cinemapedia/lib/presentation/screens/movies/movie_screen.dart
@@ -54,9 +54,90 @@ class MovieScreenState extends ConsumerState<MovieScreen> {
         // con clampingscrollphysics evitamos el rebote que tiene en iOS
         physics: const ClampingScrollPhysics(),
         slivers: [
-          _CustomSliverAppBar(movie: movie)
+
+          _CustomSliverAppBar(movie: movie),
+
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+              (context, index) => _MovieDetails(movie: movie),
+              childCount: 1
+            )
+          )
+
         ],
       ),
+    );
+  }
+}
+
+class _MovieDetails extends StatelessWidget {
+
+  final Movie movie;
+
+  const _MovieDetails({ required this.movie });
+
+  @override
+  Widget build(BuildContext context) {
+
+    final size = MediaQuery.of(context).size;
+    final textStyle = Theme.of(context).textTheme;
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(8),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              // Imagen
+              ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: Image.network(
+                  movie.posterPath,
+                  width: size.width * 0.3,
+                ),
+              ),
+
+              const SizedBox(width: 10),
+
+              // Description
+              SizedBox(
+                width: ( size.width - 40 ) * 0.7,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(movie.title, style: textStyle.titleLarge),
+                    Text(movie.overview)
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+
+        // Generos de la pelicula
+        Padding(
+          padding: const EdgeInsets.all(8),
+          child: Wrap(
+            children: [
+              ...movie.genreIds.map((gender) => Container(
+                margin: const EdgeInsets.only( right: 10 ),
+                child: Chip(
+                  label: Text(gender),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular( 20 )
+                  ),
+                ),
+              ))
+            ],
+          ),
+        ),
+
+        // TODO: Mostrar actores de la pelicula
+
+        const SizedBox(height: 100)
+      ],
     );
   }
 }
@@ -138,3 +219,4 @@ class _CustomSliverAppBar extends StatelessWidget {
     );
   }
 }
+
