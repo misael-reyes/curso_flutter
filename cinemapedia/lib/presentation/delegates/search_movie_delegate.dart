984296e29b1112f45040dda27d9c5f46549c45a6
misael-reyes <misaelstark11@gmail.com>
2023-05-25 19:34:38 -0600

    Construir sugerencias - peticon http

--- cinemapedia/lib/presentation/delegates/search_movie_delegate.dart
+++ cinemapedia/lib/presentation/delegates/search_movie_delegate.dart
@@ -3,7 +3,15 @@ import 'package:animate_do/animate_do.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
 import 'package:flutter/material.dart';
 
+typedef SearchMoviesCallback = Future<List<Movie>> Function( String query );
+
 class SearchMovieDelegate extends SearchDelegate<Movie?> {
+
+  final SearchMoviesCallback searchMovies;
+
+  SearchMovieDelegate({
+    required this.searchMovies
+  });
 
   @override
   String? get searchFieldLabel => 'Buscar película';
@@ -39,9 +47,29 @@ class SearchMovieDelegate extends SearchDelegate<Movie?> {
     return const Text('BuildResults');  
   }
 
+  // buildSuggestions se ejecutara cada que se escribe en el buscador
   @override
   Widget buildSuggestions(BuildContext context) {
-    return const Text('BuildSuggestions'); 
+    // Necesitamos crear el widget a partir de un Future, por eso usamos FutureBuilder
+    return FutureBuilder(
+      future: searchMovies( query ),
+      builder: (context, snapshot) {
+
+        final movies = snapshot.data ?? [];
+
+        return ListView.builder(
+          itemCount: movies.length,
+          itemBuilder: (context, index) {
+
+            final movie = movies[index];
+
+            return ListTile(
+              title: Text(movie.title),
+            );
+          },
+        );
+      },
+    );
   }
 
 }
--- cinemapedia/lib/presentation/widgets/shared/custom_appbar.dart
+++ cinemapedia/lib/presentation/widgets/shared/custom_appbar.dart
@@ -1,12 +1,14 @@
 import 'package:cinemapedia/presentation/delegates/search_movie_delegate.dart';
+import 'package:cinemapedia/presentation/providers/providers.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class CustomAppbar extends StatelessWidget {
+class CustomAppbar extends ConsumerWidget {
 
   const CustomAppbar({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
 
     final colors = Theme.of(context).colorScheme;
     final titleStyle = Theme.of(context).textTheme.titleMedium;
@@ -26,10 +28,15 @@ class CustomAppbar extends StatelessWidget {
               const Spacer(),
               IconButton(
                 onPressed: (){
+
+                  final movieRepository = ref.read( movieRepositoryProvider );
+
                   showSearch(
                     context: context, 
                     // el delegate es el encargado de trabajar la busqueda
-                    delegate: SearchMovieDelegate()
+                    delegate: SearchMovieDelegate(
+                      searchMovies: movieRepository.searchMovies
+                    )
                   );
                 }, 
                 icon: const Icon(Icons.search)
