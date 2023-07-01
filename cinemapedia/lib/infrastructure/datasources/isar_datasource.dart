0c0a559e7fa2abaed0d78fe607206e0c5f07af91
misael-reyes <misaelstark11@gmail.com>
2023-07-01 12:24:55 -0600

    Isar Datasource Implementation

--- cinemapedia/lib/infrastructure/datasources/isar_datasource.dart
+++ cinemapedia/lib/infrastructure/datasources/isar_datasource.dart
@@ -1,8 +1,34 @@
 
 import 'package:cinemapedia/domain/datasources/local_storage_datasource.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:isar/isar.dart';
+import 'package:path_provider/path_provider.dart';
 
 class IsarDatasource extends LocalStorageDatasource {
+
+  // lo usamos con late porque la bd tiene que estar lista antes de hacer
+  // cualquier acción
+  late Future<Isar> db;
+
+  // constructor
+  IsarDatasource() {
+    db = openDB();
+  }
+
+  Future<Isar> openDB() async {
+    
+    final dir = await getApplicationDocumentsDirectory();
+    
+    if(Isar.instanceNames.isEmpty) { // si no tenemos ninguna instancia
+      // los esquemas son las entidades que generamos
+      return await Isar.open(
+        [ MovieSchema ], 
+        inspector: true, // nos permitirá tener un servicio para ver como esta la bd
+        directory: dir.path
+      );
+    }
+    return Future.value(Isar.getInstance());
+  }
   
   @override
   Future<bool> isMovieFavorite(int movieId) {
